240f425b13143fb17d62e7bbf8589e3177cbbc4e
unknown <daniel.camposdev@gmail.com>
2021-02-15 21:52:13 -0400

    Test my first github actions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send("Hello Github Actions")
+    return res.send("Hello my first Github Actions")
 });
 
 app.listen(process.env.PORT || 5000, () => {
